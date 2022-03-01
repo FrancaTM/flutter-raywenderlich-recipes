712d8c0e36862a5385ec84e0e6d136138e494ad5
FrancaTM <francatm@outlook.com>
2022-03-01 13:18:21 -0300

    add serving slider

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -16,7 +16,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -51,8 +51,20 @@ class _RecipeDetailState extends State<RecipeDetail> {
                 },
               ),
             ),
-
-            // TODO: Add Slider() here
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
